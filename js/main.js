476e00364b02acbadbd30bf80e68e850265428c0
Milenko Stanković <stankovicm1999@gmail.com>
2022-02-21 11:59:38 +0100

    Main Sectino Mobile Done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 let btnHam = document.querySelector('#btnHam');
 let imgIconHamburger = document.querySelector('#imgIconHamburger');
+let anishaBtn = document.querySelector('#anishaBtn');
+let aliBtn = document.querySelector('#aliBtn');
+let richardBtn = document.querySelector('#richardBtn');
+let shanaiBtn = document.querySelector('#shanaiBtn');
+let anishaDiv = document.querySelector('.anishaDiv');
+let aliDiv = document.querySelector('.aliDiv');
+let richardDiv = document.querySelector('.richardDiv');
+let shanaiDiv = document.querySelector('.shanaiDiv');
 
 
 
@@ -11,4 +19,53 @@ function changeBtnHamIcon() {
   } else {
     imgIconHamburger.src = 'images/icon-hamburger.svg';
   }
+}
+
+anishaBtn.addEventListener('click', changeAnisha);
+aliBtn.addEventListener('click', changeAli);
+richardBtn.addEventListener('click', changeRichard);
+shanaiBtn.addEventListener('click', changeShanai);
+
+function changeAnisha() {
+  anishaDiv.classList.remove('customerHide');
+  anishaBtn.classList.add('btnCustomerActive');
+  aliDiv.classList.add('customerHide');
+  richardDiv.classList.add('customerHide');
+  shanaiDiv.classList.add('customerHide');
+  aliBtn.classList.remove('btnCustomerActive');
+  richardBtn.classList.remove('btnCustomerActive');
+  shanaiBtn.classList.remove('btnCustomerActive');
+}
+
+function changeAli() {
+  aliDiv.classList.remove('customerHide');
+  aliBtn.classList.add('btnCustomerActive');
+  anishaDiv.classList.add('customerHide');
+  richardDiv.classList.add('customerHide');
+  shanaiDiv.classList.add('customerHide');
+  anishaBtn.classList.remove('btnCustomerActive');
+  richardBtn.classList.remove('btnCustomerActive');
+  shanaiBtn.classList.remove('btnCustomerActive');
+}
+
+function changeRichard() {
+  richardDiv.classList.remove('customerHide');
+  richardBtn.classList.add('btnCustomerActive');
+  anishaDiv.classList.add('customerHide');
+  aliDiv.classList.add('customerHide');
+  shanaiDiv.classList.add('customerHide');
+  anishaBtn.classList.remove('btnCustomerActive');
+  aliBtn.classList.remove('btnCustomerActive');
+  shanaiBtn.classList.remove('btnCustomerActive');
+}
+
+function changeShanai() {
+  shanaiDiv.classList.remove('customerHide');
+  shanaiBtn.classList.add('btnCustomerActive');
+  anishaDiv.classList.add('customerHide');
+  aliDiv.classList.add('customerHide');
+  richardDiv.classList.add('customerHide');
+  anishaBtn.classList.remove('btnCustomerActive');
+  aliBtn.classList.remove('btnCustomerActive');
+  richardBtn.classList.remove('btnCustomerActive');
 }
